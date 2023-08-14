7b231da3fbfde1ff2ab7a44a14d2d79daab817d3
alfadev1 <alfarollano@gmail.com>
2023-08-14 00:08:36 -0300

    agregando ej 1;2

---
+++ ejercicio1Guia2e3/src/ejercicio1guia2e3/Ejercicio1Guia2e3.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio1guia2e3;
+
+/**
+ *
+ * @author cyka
+ */
+public class Ejercicio1Guia2e3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
