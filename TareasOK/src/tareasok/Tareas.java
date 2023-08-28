a4b6693691ddb674bb923a9cee1b63dc591abbda
alfadev1 <alfarollano@gmail.com>
2023-08-28 17:09:22 -0300

    agregado ejercicio extra

---
+++ TareasOK/src/tareasok/Tareas.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tareasok;
+
+/**
+ *
+ * @author cyka
+ */
+public class Tareas {
+    private String descripcion;
+    private String estado;
+
+    public Tareas(String descripcion, String estado) {
+        this.descripcion = descripcion;
+        this.estado = estado;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+    
+    
+}
